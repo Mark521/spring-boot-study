00e1f3b6d0b2e798126e6f008217a09a0908466c
Mark521 <a1092175232@163.com>
2018-07-17 00:10:06 +0800

    新增了lombok工具类

---
+++ demo/src/main/java/com/mark/demo/model/Car.java
@@ -0,0 +1,17 @@
+package com.mark.demo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    private String name;
+    private Integer year;
+}
